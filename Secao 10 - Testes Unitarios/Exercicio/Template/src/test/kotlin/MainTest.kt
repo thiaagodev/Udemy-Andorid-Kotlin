a407e916e0fb6d7516e27624713040990989cfaf
Thiago Ferreira <thiaago.dev@gmail.com>
2022-08-25 19:54:21 -0300

    cobrindo 100% das linhas da função portaria com testes unitários

--- Secao 10 - Testes Unitarios/Exercicio/Template/src/test/kotlin/MainTest.kt
+++ Secao 10 - Testes Unitarios/Exercicio/Template/src/test/kotlin/MainTest.kt
@@ -1,3 +1,20 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class MainTest {
+    @Test
+    @DisplayName("Testa os cenarios da Portaria")
+    fun testPortaria() {
+        Assertions.assertAll(
+            { Assertions.assertEquals(portaria(15, "", ""), "Negado.")},
+            { Assertions.assertEquals(portaria(20, "", ""), "Negado.")},
+            {Assertions.assertEquals(portaria(25, "VIP", ""), "Negado.")},
+            {Assertions.assertEquals(portaria(25, "comum", "xt"), "Welcome.")},
+            {Assertions.assertEquals(portaria(25, "comum", "xl"), "Negado.")},
+            {Assertions.assertEquals(portaria(25, "premium", "xl"), "Welcome.")},
+            {Assertions.assertEquals(portaria(25, "luxo", "xl"), "Welcome.")}
+        )
+    }
 
 }
